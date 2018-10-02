99ad4fc7678bd58dd7c8c152f17b51356a5207a6
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-10-02 18:12:08 -0500

    mvp deployment/

--- desparche/src/modules/Features.js
+++ desparche/src/modules/Features.js
@@ -14,31 +14,31 @@ export default class Features extends Component {
         <div className="feature">
             <img className='featureImage' src={Discover} alt="Discover"/>
             <h3>Discover</h3>
-            <p className='featureDescription'>Descubre planes dentro de alsdalksdjlaksdj laskdjalksdas .</p>
+            <p className='featureDescription'>¡Descubre planes de tiempo limitado y ofertas especiales!</p>
         </div>
 
         <div className="feature">
             <img className='featureImage' src={Plans} alt="Plans"/>
             <h3>Plans</h3>
-            <p className='featureDescription'>Descubre planes dentro de alsdalksdjlaksdj laskdjalksdas .</p>
+            <p className='featureDescription'>Héchale un vistazo a los planes disponibles en Bogotá.</p>
         </div>
 
         <div className="feature">
             <img className='featureImage' src={Search} alt="Search"/>
             <h3>Search</h3>
-            <p className='featureDescription'>Descubre planes dentro de alsdalksdjlaksdj laskdjalksdas .</p>
+            <p className='featureDescription'>Busca tus planes. <b>Personalizados</b> a tu medida.</p>
         </div>
 
         <div className="feature">
             <img className='featureImage' src={Favorites} alt="Favorites"/>
             <h3>Favorites</h3>
-            <p className='featureDescription'>Descubre planes dentro de alsdalksdjlaksdj laskdjalksdas .</p>
+            <p className='featureDescription'>Guarda los planes que mas te gusten.</p>
         </div>
 
         <div className="feature">
             <img className='featureImage' src={Tourist} alt="Tourist"/>
             <h3>Tourist</h3>
-            <p className='featureDescription'>Descubre planes dentro de alsdalksdjlaksdj laskdjalksdas .</p>
+            <p className='featureDescription'>¿Te gustaría ser un turista tu ciudad?</p>
         </div>
       </div>
     )
