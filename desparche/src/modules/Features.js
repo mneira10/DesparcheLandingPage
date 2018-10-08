f3824c7e0c57136e0a1983c2dcc1382c167c736b
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-10-08 07:51:06 -0500

    page is responsive

--- desparche/src/modules/Features.js
+++ desparche/src/modules/Features.js
@@ -4,6 +4,7 @@ import Plans from '../imgs/plans.svg';
 import Search from '../imgs/search.svg';
 import Favorites from '../imgs/favorites.svg';
 import Tourist from '../imgs/tourist.svg';
+import "./features.css";
 
 
 export default class Features extends Component {
@@ -11,34 +12,38 @@ export default class Features extends Component {
     return (
       <div className='Features'>
 
-        <div className="feature">
-            <img className='featureImage' src={Discover} alt="Discover"/>
-            <h3>Discover</h3>
-            <p className='featureDescription'>¡Descubre planes de tiempo limitado y ofertas especiales!</p>
+        <div className="row">
+            <div className="feature col-sm-auto">
+                <img className='featureImage' src={Discover} alt="Discover"/>
+                <h3>Discover</h3>
+                <p className='featureDescription'>¡Descubre planes de tiempo limitado y ofertas especiales!</p>
+            </div>
+    
+            <div className="feature col-sm-auto">
+                <img className='featureImage' src={Plans} alt="Plans"/>
+                <h3>Plans</h3>
+                <p className='featureDescription'>Échale un vistazo a los planes disponibles en Bogotá.</p>
+            </div>
+    
+            <div className="feature col-sm-auto">
+                <img className='featureImage' src={Search} alt="Search"/>
+                <h3>Search</h3>
+                <p className='featureDescription'>Busca tus planes. <b>Personalizados</b> a tu medida.</p>
+            </div>
         </div>
 
-        <div className="feature">
-            <img className='featureImage' src={Plans} alt="Plans"/>
-            <h3>Plans</h3>
-            <p className='featureDescription'>Échale un vistazo a los planes disponibles en Bogotá.</p>
-        </div>
-
-        <div className="feature">
-            <img className='featureImage' src={Search} alt="Search"/>
-            <h3>Search</h3>
-            <p className='featureDescription'>Busca tus planes. <b>Personalizados</b> a tu medida.</p>
-        </div>
-
-        <div className="feature">
-            <img className='featureImage' src={Favorites} alt="Favorites"/>
-            <h3>Favorites</h3>
-            <p className='featureDescription'>Guarda los planes que mas te gusten.</p>
-        </div>
-
-        <div className="feature">
-            <img className='featureImage' src={Tourist} alt="Tourist"/>
-            <h3>Tourist</h3>
-            <p className='featureDescription'>¿Te gustaría ser un turista tu ciudad?</p>
+        <div className="row">
+            <div className="feature col-sm-auto">
+                <img className='featureImage' src={Favorites} alt="Favorites"/>
+                <h3>Favorites</h3>
+                <p className='featureDescription'>Guarda los planes que mas te gusten.</p>
+            </div>
+    
+            <div className="feature col-sm-auto">
+                <img className='featureImage' src={Tourist} alt="Tourist"/>
+                <h3>Tourist</h3>
+                <p className='featureDescription '>¿Te gustaría ser un turista tu ciudad?</p>
+            </div>
         </div>
       </div>
     )
--- desparche/src/modules/MainBanner.js
+++ desparche/src/modules/MainBanner.js
@@ -6,14 +6,18 @@ export default class MainBanner extends Component {
   render() {
     return (
       <div className='MainBanner'>
-        <div className='MainBannerItem titles'>
-            <h1>Desparche</h1>
-            <p>¿Qué hacer?</p>
-            <p>Nosotros sabemos.</p>
-        </div>
-        
-        <div className='MainBannerItem'>
-            <img className='phonePic' src={phonePic} alt="The App"/>
+        <div className="row MainBannerContainer">
+          <div className='MainBannerItem titles col-sm-auto col-lg-6'>
+              <h1>Desparche</h1>
+              <p>¿Qué hacer?</p>
+              <p>Nosotros sabemos.</p>
+          </div>
+          
+          <div className='ImageContainer col-sm-auto col-lg-6'>
+              <div className="imagePapa">
+                <img className='phonePic' src={phonePic} alt="The App"/>
+              </div>
+          </div>
         </div>
         
         
